93e7b12d4f1fc72c06e52c01f56f2ebd894a7901
Vicente Silva Vera <ve.silvavera@gmail.com>
2022-11-05 04:03:55 -0300

    optimizacion de la estructura

--- fmindex.cpp
+++ fmindex.cpp
@@ -1,27 +1,43 @@
+#include <bits/stdc++.h>
 #include "fmindex.h"
-#include <algorithm>
 using namespace std;
 
-string FMIndex::BWTC(string source, map<char,int> &c){
-    string L;
-    source.push_back(char(4));
-    vector<string> rotations;
-    for (int i = 0; i < source.size(); ++i)
-    {
-        rotations.push_back(source);
-        rotate(source.begin(), source.begin() + 1, source.end());
-    }
-    sort(rotations.begin(),rotations.end());
 
-    for (int i = 0; i < source.size(); ++i)
-    {
-        L.push_back(rotations[i][source.size()-1]);
-        c[rotations[i][source.size()-1]]++;
+string FMIndex::BWTC(const string &source, map<char,int> &c){
+    string L;
+    string t = source;
+    vector<unsigned> _arr(source.size()+1);
+    t.push_back('$');
+	string_view sv = string_view(t);
+	iota(_arr.begin(),_arr.end(),0);
+	sort(_arr.begin(), _arr.end(), //se ordenan los indices segun la funcion creada abajo ordenando asi los sufijos por alfabeto
+			[sv](unsigned l, unsigned r) -> bool{
+				return sv.substr(l)< sv.substr(r);
+			}
+		); 
+    for (int i = 0; i < t.size(); ++i){
+    	int j = _arr[i] - 1;
+    	if(j < 0 )j += t.size();
+        L.push_back(t[j]);
+        c[t[j]]++;
+
+        if(!preOcc[t[j]].size()){
+        	for (int l = 0; l < source.size()/m + 2; ++l)
+        		preOcc[t[j]].push_back(0);
+        }
+
+        if(i%m == 0 and i != 0){
+        	map<char, vector<int>>::iterator it;
+        	for(it = preOcc.begin(); it !=preOcc.end(); it++){
+        		it->second[i/m + 1] = it->second[i/m];
+        	}
+        }
+        preOcc[t[j]][i/m + 1]++; 
     }
+
     map<char,int>::iterator it;
     int sum = 0,current = 0;
-    for (it = c.begin(); it != c.end(); ++it)
-    {
+    for (it = c.begin(); it != c.end(); ++it){
     	current = it->second;
     	it->second = sum;
     	sum += current;
@@ -30,13 +46,14 @@ string FMIndex::BWTC(string source, map<char,int> &c){
 }
 
 int FMIndex::Occ(char c, int k){
-	int sum = 0;
-	for (int i = 0; i < k; ++i)
-	{
-		if(L[i]==c)sum++;
+	int i = k/m;
+	int sum = preOcc[c][i];
+	for (int j = i*m ; j < k; ++j){
+		if(L[j]==c)sum++;
 	}
 	return sum;
 }
+
 
 int FMIndex::LFmapping(int i){
 	return C[L[i]] + Occ(L[i],i);
@@ -67,6 +84,12 @@ int FMIndex::count(const string &s){
 	return 0;
 }
 
+void FMIndex::displayL(){
+	cout<<L<<endl;
+}
+
 FMIndex::FMIndex(const string &T){
+	m = 1000;
 	L = BWTC(T,C);
+	
 }
--- fmindex.h
+++ fmindex.h
@@ -1,16 +1,21 @@
+#include <string>
+#include <vector>
 using namespace std;
 
 class FMIndex {
 private:
+	int m;
 	string L;
 	map<char,int> C;
-	string BWTC(string source, map<char,int> &c);//se obtiene el BWT y el C
-	
+	map<char,vector<int>> preOcc;
+	string BWTC(const string &source, map<char,int> &c);//se obtiene el BWT y el C
 	int LFmapping(int i);
+	int Occ(char c, int k);
 
 public:
-	int Occ(char c, int k);
+	
 	FMIndex(const string &T);
 	string extract();
 	int count(const string &s);
+	void displayL();
 };
