aa403c7cfda6717e2817226a2d797931afbcfb9b
Vicente Silva Vera <ve.silvavera@gmail.com>
2022-11-05 18:25:08 -0300

    optimizacion ligera SA

--- SuffixArray.hpp
+++ SuffixArray.hpp
@@ -12,8 +12,6 @@ class SuffixArray{
         string_view _sv; //Es como un puntero pero sin almacenamiento
         vector<unsigned> _arr;
 
-        bool compareSuffix(unsigned l,unsigned r);
-
 	public:
 		SuffixArray(const string& s);
 		unsigned countMatches(const string& s);
